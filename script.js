08fb800b8a12624b7bd39343b89d73a61f5a247c
krishna684 <50433347+krishna684@users.noreply.github.com>
2024-08-17 22:07:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup(certId) {
+    document.getElementById(certId).style.display = "block";
+}
+
+function closePopup(certId) {
+    document.getElementById(certId).style.display = "none";
+}
